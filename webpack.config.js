14622a279464b25d417b9c370ccdb153b7bd3873
Chester H <chesterh@chesters-mbp.lan>
2020-03-30 15:35:17 -0700

    create dev environment

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+    entry: [
+        './index.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ["@babel/preset-env", "@babel/react"]
+                }
+            },
+        ],
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 3000
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+    ]
+}
